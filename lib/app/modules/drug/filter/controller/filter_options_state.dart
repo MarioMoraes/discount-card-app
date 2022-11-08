d075a1b1a40e10adaa3fe2b56ab8217ad7209241
Mario Moraes <moraes_mario@hotmail.com>
2022-11-08 20:06:55 -0300

    Controller

--- lib/app/modules/drug/filter/controller/filter_options_state.dart
+++ lib/app/modules/drug/filter/controller/filter_options_state.dart
@@ -9,29 +9,40 @@ enum SearchStatus {
 }
 
 class FilterOptionsState extends Equatable {
-  final List<DrugsFilterModel> list;
+  final List<DrugsFilterModel> listCoverages;
+  final List<DrugsFilterModel> listTypes;
+  final List<DrugsFilterModel> listDosages;
+
   final SearchStatus status;
 
   const FilterOptionsState._({
-    required this.list,
+    required this.listCoverages,
+    required this.listTypes,
+    required this.listDosages,
     required this.status,
   });
 
   FilterOptionsState.initial()
       : this._(
-          list: [],
+          listCoverages: [],
+          listTypes: [],
+          listDosages: [],
           status: SearchStatus.initial,
         );
 
   @override
-  List<Object?> get props => [list, status];
+  List<Object?> get props => [listCoverages, listTypes, listDosages, status];
 
   FilterOptionsState copyWith({
-    List<DrugsFilterModel>? list,
+    List<DrugsFilterModel>? listCoverages,
+    List<DrugsFilterModel>? listTypes,
+    List<DrugsFilterModel>? listDosages,
     SearchStatus? status,
   }) {
     return FilterOptionsState._(
-      list: list ?? this.list,
+      listCoverages: listCoverages ?? this.listCoverages,
+      listTypes: listTypes ?? this.listTypes,
+      listDosages: listDosages ?? this.listDosages,
       status: status ?? this.status,
     );
   }
